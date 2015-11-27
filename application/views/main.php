7178f422b807685a00fb612306e52afd3c92b8aa
OA Wu <comdan66@gmail.com>
2015-11-27 11:51:58 +0800

    Add og info.

--- application/views/main.php
+++ application/views/main.php
@@ -5,7 +5,30 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
 
-        <title>地圖</title>
+        <title>OA's point in polygon demo!</title>
+
+        <meta name="robots" content="index,follow" />
+
+        <meta name="author" content="吳政賢(OA Wu)" />
+
+        <meta name="keywords" content="point in polygon | OA | Google Maps API | jQuery" />
+        <meta name="description" content="就是一個多邊形，拉出範圍，選出範圍內的項目。" />
+        <meta property="og:site_name" content="OA's point in polygon demo!" />
+        <meta property="og:title" content="OA's point in polygon demo!" />
+        <meta property="og:description" content="就是一個多邊形，拉出範圍，選出範圍內的項目。" />
+        <meta property="og:url" content="http://polygon.ioa.tw/" />
+
+        <meta property="fb:admins" content="100000100541088" />
+        <meta property="fb:app_id" content="640377126095413" />
+
+        <meta property="og:locale" content="zh_TW" />
+        <meta property="og:locale:alternate" content="en_US" />
+
+        <meta property="og:type" content="city" />
+        <meta property="og:image" content="/assets/img/og.png" alt="OA's point in polygon demo!" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
 
         <link href="/assets/css/main.css" rel="stylesheet" type="text/css" />
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&language=zh-TW&libraries=places" language="javascript" type="text/javascript"></script>
